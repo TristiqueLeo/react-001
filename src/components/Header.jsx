2e39622865c837f728b0bd357e8c292a37bdbdc5
Ashim Sherkhan <ashim.sherkhan02@gmail.com>
2023-01-17 08:43:40 +0600

    asd

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlwSmkcgmEDGtftq6x7CDxqp-vZMndjoFgOI2sNtubrQ&s' />
+    </header>
+}
+
+export default Header; 
