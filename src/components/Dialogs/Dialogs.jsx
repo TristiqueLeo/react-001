af872d2a98c01168e983aa9d6dfb3387e8d68976
Ashim Sherkhan <ashim.sherkhan02@gmail.com>
2023-02-22 23:48:08 +0600

    asd

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,7 +7,7 @@ import Message from './Message/Message';
 let dialogsData = [
     { id: 1, name: 'One' },
     { id: 2, name: 'Two' },
-    { id: 3, name: 'Three' },
+    { id: 47, name: 'Three' },
     { id: 4, name: 'Four' },
     { id: 5, name: 'Five' },
     { id: 6, name: 'Six' },
